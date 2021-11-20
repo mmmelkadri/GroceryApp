940628b65297f78d1a8be92cf6f9494fe5edc608
melkadri <mmmelkadri@gmail.com>
2021-11-20 07:16:09 -0500

    Created checkout and orders xml layouts

---
+++ app/src/main/java/com/example/groceryapp/CustomerOrderActivity.java
@@ -0,0 +1,14 @@
+package com.example.groceryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_order);
+    }
+}
