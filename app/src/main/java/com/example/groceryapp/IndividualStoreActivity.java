204591d5359ee50af9fcff5f146162c12f4291c9
Ahsan Saeed <ahsan02@gmail.com>
2021-11-20 22:57:03 -0500

    Created Individual Store, Personal Store and Pending Order pages' layouts.

---
+++ app/src/main/java/com/example/groceryapp/IndividualStoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.groceryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndividualStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_store);
+    }
+}
