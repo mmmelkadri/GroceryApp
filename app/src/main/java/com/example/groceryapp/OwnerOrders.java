ecec66b080112e88d9e031b5d444423f1c473bdf
elnagne <40046289+elnagne@users.noreply.github.com>
2021-11-20 20:15:15 -0500

    Create new item xml
    New Item xml for owner page

---
+++ app/src/main/java/com/example/groceryapp/OwnerOrders.java
@@ -0,0 +1,14 @@
+package com.example.groceryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_orders);
+    }
+}
