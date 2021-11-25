20db8bbde78175c7a6ab8524da2163dcdd2e31f6
mmmelkadri <mmmelkadri@gmail.com>
2021-11-25 02:41:08 -0500

    Pages 5, 6, 7 and Reader class

--- app/src/main/java/com/example/groceryapp/CheckoutActivity.java
+++ app/src/main/java/com/example/groceryapp/CheckoutActivity.java
@@ -3,6 +3,7 @@ package com.example.groceryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -10,5 +11,24 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        // Read users cart in database and add buttons to the scroll layout
+    }
+
+    public void incrementItem(View view) {
+        // Write to cart database, item amount + 1
+    }
+
+    public void decrementItem(View view) {
+        // Write to cart database, item amount - 1
+        // if amount is already 1, do nothing
+    }
+
+    public void removeItem(View view) {
+        // Look in cart database and remove item
+    }
+
+    public void checkout(View view) {
+        // Write order to both owner and customer
     }
 }
