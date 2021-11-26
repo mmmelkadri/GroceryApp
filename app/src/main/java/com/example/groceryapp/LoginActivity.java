8f5524846d499fcf972235b35e18c75c75438366
elnagne <40046289+elnagne@users.noreply.github.com>
2021-11-26 01:50:42 -0500

    added sign up button to write username, password and public name to database

--- app/src/main/java/com/example/groceryapp/LoginActivity.java
+++ app/src/main/java/com/example/groceryapp/LoginActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +15,34 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void SignUpSignUpButton (View view){
+        EditText username_edit =  (EditText) findViewById(R.id.Signup_Username_EditText);
+        String username_s = username_edit.getText().toString();
+
+        EditText password_edit =  (EditText) findViewById(R.id.Signup_Password_EditText);
+        String password_s = password_edit.getText().toString();
+
+        EditText public_name_edit =  (EditText) findViewById(R.id.Signup_PublicName_EditText);
+        String public_s = public_name_edit.getText().toString();
+
+        Spinner spinner = (Spinner) findViewById(R.id.Signup_UserDropdown);
+        String spinner_s = String.valueOf(spinner.getSelectedItem());
+
+        try {
+            if (spinner_s.equals("Owner")) {
+                Owner owner = new Owner(password_s, public_s);
+                owner.writeToDatabase(username_s);
+                Toast.makeText(LoginActivity.this,"done", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Customer customer = new Customer(password_s, public_s);
+                customer.writeToDatabase(username_s);
+            }
+        }
+        catch(Exception e){
+            Toast.makeText(LoginActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/groceryapp/Owner.java
+++ app/src/main/java/com/example/groceryapp/Owner.java
@@ -1,15 +1,23 @@
 package com.example.groceryapp;
 
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class Owner extends User{
-
-    public Owner(String username, String password, String display_name){
-        this.username = username;
+    ArrayList<String> orders;
+    ArrayList<Object> products;
+    public Owner(String password, String display_name){
         this.password = password;
         this.display_name = display_name;
+    }
+
+    public void writeToDatabase(String user_ID){
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("Owners").child(user_ID).child("Public Name").setValue(this.display_name);
+        mDatabase.child("Owners").child(user_ID).child("Password").setValue(this.password);
     }
 
     public ArrayList<Object> getOrders(String user_ID) {
