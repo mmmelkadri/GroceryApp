fbe6c4f647e64636f2ddddf3844e02857c077097
melkadri <mmmelkadri@gmail.com>
2021-11-27 18:44:16 -0500

    Previous build didn't take into account that DataSnapshot would be changed.
    Added onCompleteListener to .get() to fix this issue.

--- app/src/main/java/com/example/groceryapp/Reader.java
+++ app/src/main/java/com/example/groceryapp/Reader.java
@@ -1,5 +1,10 @@
 package com.example.groceryapp;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -11,12 +16,11 @@ public class Reader {
      * Generic FireBase reader *
      * ************************/
 
-    Task<DataSnapshot> databaseSnapshot;
+    DatabaseReference mDatabase;
     public static Reader read;
 
     private Reader() {
-        // .get() is asynchronous, so we need to set it up ahead of time
-        databaseSnapshot = FirebaseDatabase.getInstance().getReference().get();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
     public static Reader getInstance() {
@@ -27,6 +31,20 @@ public class Reader {
 
     public String readValue(String... args) {
         // Pass in keys path from mDatabase root to get value
+
+        // .get() is asynchronous, so we need to wait for the database
+        Task<DataSnapshot> databaseSnapshot = mDatabase.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+
         DataSnapshot temp = databaseSnapshot.getResult();
 
         for (String arg : args) {
@@ -42,6 +60,20 @@ public class Reader {
 
     public DataSnapshot readSnapshot(String... args) {
         // Pass in keys path from mDatabase root to get snapshot at final key
+
+        // .get() is asynchronous, so we need to wait for the database
+        Task<DataSnapshot> databaseSnapshot = mDatabase.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+
         DataSnapshot temp = databaseSnapshot.getResult();
 
         for (String arg : args) {
