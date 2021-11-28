bd3ccc602010ac3f7df2ab843a0d3ff142d849ee
Aditya Kulkarni <65531635+Aditya-k-23@users.noreply.github.com>
2021-11-28 11:56:42 -0500

    Interchanged the login and signup activity

--- app/src/main/java/com/example/groceryapp/LoginActivity.java
+++ app/src/main/java/com/example/groceryapp/LoginActivity.java
@@ -14,34 +14,12 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
     }
 
-    public void SignUpSignUpButton (View view){
-        EditText username_edit =  (EditText) findViewById(R.id.Signup_Username_EditText);
-        String username_s = username_edit.getText().toString();
-
-        EditText password_edit =  (EditText) findViewById(R.id.Signup_Password_EditText);
-        String password_s = password_edit.getText().toString();
-
-        EditText public_name_edit =  (EditText) findViewById(R.id.Signup_PublicName_EditText);
-        String public_s = public_name_edit.getText().toString();
-
-        Spinner spinner = (Spinner) findViewById(R.id.Signup_UserDropdown);
-        String spinner_s = String.valueOf(spinner.getSelectedItem());
-
-        try {
-            if (spinner_s.equals("Owner")) {
-                Owner owner = new Owner(username_s, password_s, public_s);
-                owner.writeToDatabase();
-            }
-            else {
-                Customer customer = new Customer(username_s, password_s, public_s);
-                customer.writeToDatabase();
-            }
-        }
-        catch(Exception e){
-            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
+    public void SignUpButton (View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/groceryapp/SignUpActivity.java
+++ app/src/main/java/com/example/groceryapp/SignUpActivity.java
@@ -5,19 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
     }
 
 
 
-    public void SignUpButton (View view){
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+
+    public void SignUpSignUpButton (View view){
+        EditText username_edit =  (EditText) findViewById(R.id.Signup_Username_EditText);
+        String username_s = username_edit.getText().toString();
+
+        EditText password_edit =  (EditText) findViewById(R.id.Signup_Password_EditText);
+        String password_s = password_edit.getText().toString();
+
+        EditText public_name_edit =  (EditText) findViewById(R.id.Signup_PublicName_EditText);
+        String public_s = public_name_edit.getText().toString();
+
+        Spinner spinner = (Spinner) findViewById(R.id.Signup_UserDropdown);
+        String spinner_s = String.valueOf(spinner.getSelectedItem());
+
+        try {
+            if (spinner_s.equals("Owner")) {
+                Owner owner = new Owner(username_s, password_s, public_s);
+                owner.writeToDatabase();
+            }
+            else {
+                Customer customer = new Customer(username_s, password_s, public_s);
+                customer.writeToDatabase();
+            }
+        }
+        catch(Exception e){
+            Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
